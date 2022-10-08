a18817870b065ab618d69630b0903e5407e4235f
wasuwa <wasuwa911@gmail.com>
2022-10-08 18:43:43 +0900

    header の文字を h2 から p に変更する

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
   return (
     <header className='border-b border-solid border-primary-border bg-primary-content'>
-      <h2 className='text-primary text-2xl font-bold px-one py-3'>ToDo App</h2>
+      <p className='text-primary text-2xl font-bold px-one py-3'>ToDo App</p>
     </header>
   );
 };
