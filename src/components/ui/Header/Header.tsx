2e56449b9f54e1e6514119074fcf501176cb9396
wasuwa <wasuwa911@gmail.com>
2022-10-08 20:43:39 +0900

    header の文字をリンクにする

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <header className='border-b border-solid border-primary-border bg-primary-content'>
-      <p className='text-primary text-2xl font-bold px-one py-3'>ToDo App</p>
+      <Link to="/" className='block text-primary text-2xl font-bold px-one py-3'>ToDo App</Link>
     </header>
   );
 };
